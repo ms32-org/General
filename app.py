8af1a54563bae40f7eb7468cd16949ddffa4acd9
ms32-org <ms32-org@outlook.com>
2025-01-05 21:49:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -481,7 +481,36 @@ def terminal():
                 print("---------output stored-----------")		    
             
     return "done"
-    
+@app.route("/control", methods=["GET", "POST"])
+def control():
+    global control_data
+    if request.method == "POST":
+        data = request.get_json()
+        if data["type"] == "key":
+            control_data["type"] = "key"
+            control_data["btn"] = data["button"]
+        elif data["type"] == "click":
+            control_data["type"] = "mouse"
+            control_data["x"] = data["x"]
+            control_data["y"] = data["y"]
+            control_data["mouse"] = data["button"]
+            control_data["width"] = data["width"]
+            control_data["height"] = data["height"]
+        elif data["type"] == "scroll":
+            control_data["type"] = "scroll"
+            control_data["deltaY"] = data["deltaY"]
+        elif data["type"] == "dbclick":
+            control_data["type"] = "dbclick"
+            control_data["x"] = data["x"]
+            control_data["y"] = data["y"]
+            control_data["width"] = data["width"]
+            control_data["height"] = data["height"]
+        return "done"
+
+    if request.method == "GET":
+        data1 = control_data
+        control_data = {}      
+        return jsonify(data1)             
 @app.route("/get-output",methods=["GET","POST"])
 def get_output():
 	global output
