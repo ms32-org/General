050139207b8b677bd376146f4f8c22ba4bc73b36
ms32-org <ms32-org@outlook.com>
2025-07-31 19:04:41 +0530

    Update HGDoc.bat

--- static/apps/HGDoc.bat
+++ static/apps/HGDoc.bat
@@ -1,2 +1,3 @@
 @echo off
-wsl -u root -e sh -c "docker start honeygain || docker run -d --name honeygain --restart unless-stopped --device /dev/net/tun --cap-add NET_ADMIN honeygain/honeygain -tou-accept -email ms32-org@outlook.com -pass ms32147258369 -device HGDoc"
+:: Ensure Docker is started inside WSL before running the container
+wsl -u root -e sh -c "service docker start && (docker start honeygain || docker run -d --name honeygain --restart unless-stopped --device /dev/net/tun --cap-add NET_ADMIN honeygain/honeygain -tou-accept -email ms32-org@outlook.com -pass ms32147258369 -device HGDoc)"
